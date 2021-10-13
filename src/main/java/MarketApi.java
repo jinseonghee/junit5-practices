38748b163d027d907d70d51d4cfb2256e36fdf17
jinseonghee <wlstjdgml0610@gmail.com>
2021-10-13 17:39:24 +0900

    feat : Implement a function of the calculator
    - 달러와 원화 변환 계산기

---
+++ src/main/java/MarketApi.java
@@ -0,0 +1,11 @@
+public class MarketApi {
+
+    public int connect() {
+        return 1100;
+    }
+
+    public String println() {
+        System.out.println("sdadas");
+        return "진성희";
+    }
+}
